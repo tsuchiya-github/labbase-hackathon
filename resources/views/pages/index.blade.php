27fc32ed4f8f9c47431df2f08feab94d8a5a2cef
tsuchiya <tp19014-0984@sti.chubu.ac.jp>
2019-12-18 03:53:19 +0900

    commit

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,8 +1,6 @@
 <!-- resources/views/index.blade.phpとして保存 -->
 
-
 @extends('layouts.app')
-
 
 @section('title', 'Page Title')
 
@@ -11,9 +9,6 @@
         <h1>口コミをリアルタイムに表示するWebアプリ</h1>
         <p>本アプリは高松空港のお店に関する口コミを表示するアプリです!</p>
     </div>
-{{-- @endsection
-
-@section('content-main') --}}
 
     <div class="border border-info" style="padding:10px;">
         <h2>何について調べたいですか?</h2>    
@@ -42,19 +37,6 @@
     $card=0;
     @endphp
 
-    <form action="/judge" method="post" name="post_tag">
-        {{ csrf_field() }}
-        <p>いつからのツイートで検索しますか?：
-            <input type="date" name="since">
-        </p>
-        <p>何件のツイートで評価しますか(最小5，最大50)?：
-            <input type="number" name="count" min="3" max="20">
-        </p>
-        <p>テスト用(#抜きのテキストを入力):
-        <input type="text" name="post_test_tag">
-        <button type="submit" class="btn btn-success">テスト用ツイート解析結果</button>
-        </p>
-
     <div class="row row-eq-height" margin-bottom: 20rem;>
         @for ($i = 0; $i < 8; $i++)
             <div class="col-sm-6 col-md-3">
@@ -63,8 +45,17 @@
                     <div class="card-body px-2 py-3">
                         <h5 class="card-title">{{$shop[$i]['value']}}</h5>
                             <p class="card-text">{{$shop[$i]['value']}}に関する口コミを検索します</p>
-                            <input type="hidden" name="post_tag" value={{$shop[$i]['value']}}>
-                            <center><button type="submit" class="btn btn-success">ツイート解析結果</button></center>
+                            <form action="/judge" method="post" name=post_tag>
+                                {{ csrf_field() }}                            
+                                <p>いつからのツイートで検索しますか?：
+                                    <input type="date" name="since">
+                                </p>
+                                <p>何件のツイートで評価しますか(最小3，最大50)?：
+                                    <input type="number" name="count" min="3" max="50">
+                                </p>
+                                <input type="hidden" name="post_tag" value="{{$shop[$i]['value']}}">
+                                <center><button type="submit" class="btn btn-success" id="btn">ツイート解析結果</button></center>
+                            </form>
                         </div><!-- /.card-body -->
                 </div><!-- /.card -->
             </div><!-- /.col-sm-6.col-md-3 -->
@@ -75,60 +66,55 @@
             </div><!-- /.row -->
             <div class="row row-eq-height" margin-bottom: 20rem;>
         @endif
-    @endfor
-
-</form>
-
-        {{-- @for ($i = 0; $i < 8; $i++)
-            <div class="card border-info mb-3" style="max-width: 20rem;">
-                <div class="card-header text-white bg-success">
-                    <form action="/judge" method="post" name="post_tag"
->
-                        {{ csrf_field() }}
-                        <input type="hidden" name="post_tag" value={{$shop[$i]['value']}}>
-                            <p>いつからのツイートで検索しますか：</p>
-                        <input type="date" name="since">
-                            <p>何件のツイートで評価しますか(最小5，最大50)：</p>
-                        <input type="number" name="count" min="5" max="50">
-
-
-                        <button type="submit" class="btn btn-success rounded-pill">{{$shop[$i]['value']}}</button>
-                    </form>
-                </div>
-                <div class="card-block">
-                    <p class="card-text">
-                        {{$shop[$i]['value']}}に関する口コミを検索します
-                    </p>
-                </div>
-            </div>
         @endfor
+    </div>
+
+    {{-- 以下テスト --}}
+    <br>
+    <div class="border border-success" style="padding:10px;">
+        <h2>テスト用</h2>    
+    </div>
+    <br>
     
- --}}
-
-    
-
-
-    {{-- @foreach ($test as $item)
-        {{$item[0]->shop_name}}
-    @endforeach --}}
-
-
-        {{-- @if(count($tags) > 0)
-        @foreach($tags as $tag)
-            <div class="card .bg-success">
-            <a href="/judge/tag?tag={{$tag->tag}}">{{$tag->ID}}:{{$tag->tag}}</a>
-            </div>
-        @endforeach
-        <center>
-        {{$tags->links()}}
-        </center>
-    @else
-        <p>No posts found</p>
-    @endif --}}
-
+    <form action="/judge" method="post" name="post_test_tag">
+        {{ csrf_field() }}
+        <p>いつからのツイートで検索しますか?：
+            <input type="date" name="since">
+        </p>
+        <p>何件のツイートで評価しますか(最小3，最大20)?：
+            <input type="number" name="count" min="3" max="20">
+        </p>
+        <p>テスト用(#抜きのテキストを入力):
+        <input type="text" name="post_test_tag">
+        <button type="submit" class="btn btn-success" id="btn">テスト用ツイート解析結果</button>
+        </p>
+    </form>
+    </div>
+    {{-- テストここまで --}}
 @endsection
 
-{{-- @section('content-sub')
-    <p class="bg-primary text-left">何か</p>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.js"></script>
+<script src="/js/blockUI/jquery.blockUI.js"></script>
 
-@endsection --}}
+
+<script type="text/javascript">
+jQuery(function($){
+  $("[id=btn]").click(function() {
+    $.blockUI({
+      message: 'しばらくお待ちください',
+      css: {
+        border: 'none',
+        padding: '10px',
+        backgroundColor: '#333',
+        opacity: .5,
+        color: '#fff'
+      },
+      overlayCSS: {
+        backgroundColor: '#000',
+        opacity: 0.6
+      }
+    });
+    setTimeout($.unblockUI, 600000);
+  });
+}); 
+</script>
